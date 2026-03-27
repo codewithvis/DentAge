54000473d7ada8a73eeb68316a57d02ba3c6d97f
Shashank Verma <vermashashank070205@gmail.com>
2026-03-27 13:40:47 +0530

    added forgot password and resetting URL and deep linking

--- App.js
+++ App.js
@@ -23,6 +23,7 @@ import XRayAnalysisScreen from './screens/XRayAnalysisScreen';
 import StageClassificationScreen from './screens/StageClassificationScreen';
 import ResultsDashboardScreen from './screens/ResultsDashboardScreen';
 import SettingsScreen from './screens/SettingsScreen';
+import ForgotPasswordScreen from './screens/ForgotPasswordScreen';
 
 import { isNetworkConnected } from './services/expo/network';
 import { syncOfflineData } from './services/supabase';
@@ -32,6 +33,18 @@ import ChangePasswordScreen from './screens/ChangePasswordScreen';
 import DeleteAccountScreen from './screens/DeleteAccountScreen';
 
 const Stack = createNativeStackNavigator();
+
+const linking = {
+  prefixes: ['app://'],
+  config: {
+    screens: {
+      ForgotPasswordScreen: 'reset-password',
+      Login: 'login',
+      SignUp: 'signup',
+      Home: 'home',
+    },
+  },
+};
 
 export default function App() {
   React.useEffect(() => {
@@ -45,10 +58,43 @@ export default function App() {
     initializeApp();
   }, []);
 
+  React.useEffect(() => {
+    const handleDeepLink = async (url) => {
+      if (!url) return;
+
+      const hash = url.split('#')[1];
+      if (!hash) return;
+
+      const params = Object.fromEntries(
+        hash.split('&').map((param) => param.split('='))
+      );
+
+      const access_token = params.access_token;
+      const refresh_token = params.refresh_token;
+
+      if (access_token && refresh_token) {
+        await supabase.auth.setSession({
+          access_token,
+          refresh_token,
+        });
+      }
+    };
+
+    Linking.getInitialURL().then(handleDeepLink);
+
+    const subscription = Linking.addEventListener('url', ({ url }) => {
+      handleDeepLink(url);
+    });
+
+    return () => {
+      subscription.remove();
+    };
+  }, []);
+
   return (
     <AuthProvider>
       <QueryProvider>
-        <NavigationContainer>
+        <NavigationContainer linking={linking}>
           <Stack.Navigator
             initialRouteName="Login"
             screenOptions={{ headerShown: false, animation: 'slide_from_right' }}
@@ -56,6 +102,7 @@ export default function App() {
             <Stack.Screen name="Login" component={LoginScreen} />
             <Stack.Screen name="ChangePasswordScreen" component={ChangePasswordScreen} />
             <Stack.Screen name="DeleteAccountScreen" component={DeleteAccountScreen} />
+            <Stack.Screen name="ForgotPasswordScreen" component={ForgotPasswordScreen} />
             <Stack.Screen name="SignUp" component={SignUpScreen} />
             <Stack.Screen name="Home" component={HomeScreen} />
             <Stack.Screen name="XRayAnalysis" component={XRayAnalysisScreen} />
